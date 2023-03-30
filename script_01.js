918c58422efa9f1eb30a0eb609cd0b4095955c38
Peter Weber <peter-aalen@gmx.net>
2023-03-30 10:34:19 +0200

    02 Deklaration + Wertzuweisung I

--- script_01.js
+++ script_01.js
@@ -13,13 +13,14 @@
 // console.log(firstName)
 
 
-
-
 /***** 02 Deklaration + Wertzuweisung I *******/
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung (engl. Assignment)
+console.log(firstName); // Augabe (Konsole)
 
-
-
-
+let familyName; // Deklaration (Definition)
+familyName = "Mütze"; // Wertzuweisung (engl. Assignment)
+console.log(familyName); // Augabe (Konsole)
 
 /***** 03 Deklaration + Wertzuweisung II *******/
 
